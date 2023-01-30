f35c4c13180e783af042e3f66693ab42ab33c2d8
bloodpool7 <goelr668@gmail.com>
2023-01-29 21:18:31 -0800

    New datasets

--- NNTest.py
+++ NNTest.py
@@ -4,47 +4,58 @@ from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
 from Model import *
 from ReadData import *
+import time
 
 nnfs.init()
 
-X, y = spiral_data(100, 3)
+X, y = iris_load_data()
 
-layer1 = Layer_Dense(2, 64)
-layer2 = Layer_Dense(64, 3)
+
+layer1 = Layer_Dense(4, 16)
+layer2 = Layer_Dense(16, 16)
+layer3 = Layer_Dense(16, 3)
 
 activation1 = Activation_ReLU()
-loss_activation2 = Softmax_Entropy()
+activation2 = Activation_ReLU()
+loss_activation3 = Softmax_Entropy()
 
-optimizer = Optimizer_SGD(learning_rate = 1.5, decay = 0.0002, momentum = 0.9)
+optimizer = Optimizer_SGD(learning_rate = 0.01)
 
-for epoch in range(10001):
+start = time.time()
+for epoch in range(101):
     layer1.forward(X)
     activation1.forward(layer1.outputs)
 
     layer2.forward(activation1.outputs)
+    activation2.forward(layer2.outputs)
 
-    loss = loss_activation2.forward(layer2.outputs, y)
+    layer3.forward(activation2.outputs)
+    loss = loss_activation3.forward(layer3.outputs, y)
 
-    predictions = np.argmax(loss_activation2.outputs, axis=1)
+    predictions = np.argmax(loss_activation3.outputs, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1) 
     accuracy = np.mean(predictions==y)
 
-    if not epoch % 100:
+    if not epoch % 10:
         print(f'epoch: {epoch}, ' +
               f'acc: {accuracy:.3f}, ' + 
               f'loss: {loss:.3f}, ' + 
               f'lr: {optimizer.current_learning_rate:.5f}')
     
-    loss_activation2.backward(loss_activation2.outputs, y)
-    layer2.backward(loss_activation2.dinputs)
+    loss_activation3.backward(loss_activation3.outputs, y)
+    layer3.backward(loss_activation3.dinputs)
+    activation2.backward(layer3.dinputs)
+    layer2.backward(activation2.dinputs)
     activation1.backward(layer2.dinputs)
     layer1.backward(activation1.dinputs)
 
     optimizer.pre_update_params()
     optimizer.update_params(layer1)
     optimizer.update_params(layer2)
+    optimizer.update_params(layer3)
     optimizer.post_update_params()
 
+end = time.time()
 
-
+print(f"Finished in : {end - start:.3f} seconds")
--- ReadData.py
+++ ReadData.py
@@ -1,42 +1,78 @@
 import numpy as np
 import time
 
-def load_data():
+def mnist_load_data(input_file = "Data/train-images-idx3-ubyte", label_file = "train-labels-idx1-ubyte"):
     print("Loading data...")
-    f = open("Data/train-images-idx3-ubyte", "rb")
+    f = open(input_file, "rb")
     x = f.read()
     n = int.from_bytes(x[3:4], "big") - 1
-
     length = int.from_bytes(x[4:8], "big")
     d = []
     for i in range(n):
         d.append(int.from_bytes(x[8 + i * 4: 12 + i * 4], "big"))
+    
+    d = np.array(d)
+    sample_length = np.prod(d)
 
     index = 8 + n*4
     inputs = []
     for i in range(length):
         input = []
-        for j in range(d[0] * d[1]):
+        for j in range(sample_length):
             input.append(x[index])
             index += 1
         inputs.append(input)
-    print("Finished! ")
-    return inputs
+    
+    print("Finished!\n")
+    print("Loading labels...")
+
+    f = open(label_file, "rb")
+    x = f.read()
+    n = int.from_bytes(x[3:4], "big") - 1
+    length = int.from_bytes(x[4:8], "big")
+
+    index = 8 + n*4
+    labels = []
+    for i in range(length):
+        labels.append(x[index])
+        index += 1
+    print("Finished!\n")
+    inputs = np.array(inputs)
+    labels = np.array(labels)
+    return inputs, labels
+
+def iris_load_data(input_file = "Data/iris.data"):
+    f = open(input_file, "r")
+    inputs = []
+    labels = []
+    while True:
+        line = f.readline()
+        if line and len(line) != 1:
+            line = line.split(",")
+            line[-1] = line[-1][:-1]
+            inputs.append(list(map(float, line[:-1])))
+            labels.append(0 if line[-1] == "Iris-setosa" else (1 if line[-1] == "Iris-versicolor" else 2))
+        else:
+            break
+    return np.array(inputs), np.array(labels)
 
 
 if __name__ == "__main__":
-    inputs = load_data()
-    pictures = []
-    for i in range(10):
-        picture = []
-        for j in range(len(inputs[0])):
-            picture.append(inputs[i][j])
-            if (len(picture) == 28):
-                pictures.append(picture)
-                picture = []
+    inputs, labels = iris_load_data()
+    print(inputs)
+    # inputs, labels = mnist_load_data()
+    # pictures = []
+    # for i in range(10):
+    #     picture = []
+    #     for j in range(len(inputs[0])):
+    #         picture.append(inputs[i][j])
+    #         if (len(picture) == 28):
+    #             pictures.append(picture)
+    #             picture = []
 
-    pictures = np.array(pictures)
-    np.set_printoptions(linewidth = 150)
-    for i in range(10):
-        for line in pictures[i*28:(i+1)*28]:
-            print(line)
+    # pictures = np.array(pictures)
+    # np.set_printoptions(linewidth = 150)
+    # for i in range(10):
+    #     for line in pictures[i*28:(i+1)*28]:
+    #         print(line)
+    # print(labels[:10])
