af744f9450cd39afa25f6a48d18f0e37d73d0861
bloodpool7 <goelr668@gmail.com>
2023-04-04 12:12:45 -0700

    Changing Adam

--- Model.py
+++ Model.py
@@ -221,6 +221,7 @@ class Adam(Optimizer):
         self.iterations = 0
         self.decay = decay
 
+    #To be called before any parameter updates
     def pre_update_params(self): 
         if self.decay:
             self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
