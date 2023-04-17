4ec9b310c3d80004a7b7e37e6d7cccf9927f6ab1
bloodpool7 <goelr668@gmail.com>
2023-04-17 12:12:12 -0700

    Few changes

--- NNTest.py
+++ NNTest.py
@@ -22,20 +22,10 @@ l3bias = np.array([[3, -2]]).tolist()
 weights = [l1weights, l2weights, l3weights]
 biases = [l1bias, l2bias, l3bias]
 
-l1 = Layer(3, 3)
-
-
-l2 = Layer(3, 3)
-
-
-
-l3 = Layer(3, 2)
-
-
 # Using Model Object
 
 model = Model(
-    layers = [l1, l2, l3],
+    #layers = [l1, l2, l3],
     activations = [ReLU(), ReLU(), Softmax()],
     loss = Mean_Squared_Error(),
     optimizer = SGD(learning_rate = 0.1)
