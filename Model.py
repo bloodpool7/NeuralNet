c1ca1499d980dce435c1d4d5ba464d2610aa168e
bloodpool7 <goelr668@gmail.com>
2023-04-24 18:48:01 -0700

    Defined train function for model object

--- Model.py
+++ Model.py
@@ -328,7 +328,16 @@ class Model:
     
     def train(self, X_train = None, X_valid = None, y_train = None, y_valid = None, epochs = None):
         if (len(X_train.shape) == 3):
-            pass
+            for epoch in range(epochs):
+                for batch in range(len(X_train)):
+                    self.__forward(X_train[batch], y_train[batch])
+                    self.__backward(y_train[batch])
+        elif (len(X_train.shape) == 2):
+            for epoch in range(epochs):
+                self.__forward(X_train, y_train)
+                self.__backward(y_train)
+
+        self.__forward(X_valid, y_valid)
 
     def predict(self, X, y = None) -> np.ndarray:
         self.__forward(X, y)
